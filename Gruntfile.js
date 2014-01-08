35a168215f7cf1423021d8aeaa8f52effa2d912d
Ammar Alakkad <am.alakkad@gmail.com>
2014-01-08 12:00:45 +0200

    basic grunt + bower + sass + cover image

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		watch: {
+			bs: {
+				files: "bower_components/sass-bootstrap/lib/*.scss",
+				tasks: ["sass:bs"],
+				options: {
+					livereload: true
+				}
+			},
+			dist: {
+				files: "dist/scss/*.scss",
+				tasks: ["sass:dist"],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					style: "expanded"
+				},
+				files: {
+					'assets/css/style.css': 'dist/scss/style.scss'
+				}
+			},
+			bs: {
+				files: {
+					'assets/css/bootstrap.css': 'bower_components/sass-bootstrap/lib/bootstrap.scss'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	grunt.registerTask('default', ['watch:dist']);
+};
